e3ef479461e40c6fcf2e14f5f3e6471c7193f9f0
Olesya Gorbacheva <554476744j@gmail.com>
2019-11-05 03:47:48 +0800

    binColor в шаблонах и рецептах

--- app/src/main/java/ru/buylist/pattern_list/PatternListAdapter.java
+++ app/src/main/java/ru/buylist/pattern_list/PatternListAdapter.java
@@ -12,6 +12,7 @@ import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
 import java.util.List;
 
 import ru.buylist.R;
+import ru.buylist.buy_list.CategoryInfo;
 import ru.buylist.data.entity.Item;
 import ru.buylist.databinding.ItemProductBinding;
 
@@ -104,10 +105,19 @@ public class PatternListAdapter extends RecyclerSwipeAdapter<PatternListAdapter.
             this.item = item;
             binding.setItem(item);
             binding.imgCategoryCircle.setImageResource(R.drawable.circle_empty);
+            bindColor(item);
             binding.layoutSwipeItem.setShowMode(SwipeLayout.ShowMode.PullOut);
             binding.layoutSwipeItem.addDrag(SwipeLayout.DragEdge.Right, binding.layoutBottomSwipe);
             binding.cardTopItemSwipe.setBackgroundColor(0);
             binding.executePendingBindings();
+        }
+
+        void bindColor(Item item) {
+            if (item.getCategoryColor() == null) {
+                binding.imgCategoryCircle.setColorFilter(Color.parseColor(CategoryInfo.COLOR));
+            } else {
+                binding.imgCategoryCircle.setColorFilter(Color.parseColor(item.getCategoryColor()));
+            }
         }
     }
 }
