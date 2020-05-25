37668562e75a43c384fc2d1675f193bf6b1a9584
Factis <53155715qwerty>
2020-05-25 17:09:52 +0800

    delete base activity

--- app/src/main/java/ru/buylist/presentation/SingleActivity.kt
+++ app/src/main/java/ru/buylist/presentation/SingleActivity.kt
@@ -1,12 +1,14 @@
 package ru.buylist.presentation
 
-class SingleActivity: BaseActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.buylist.R
 
-    override fun setupBottomNavigation() {
-        // TODO:
+class SingleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
     }
 
-    override fun setupDrawerMenu() {
-        // TODO:
-    }
 }
