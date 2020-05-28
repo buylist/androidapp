7490e7e5033290602f64d5f8d0ed5524a0d31342
Factis <53155715qwerty>
2020-05-28 17:19:11 +0800

    update BuyListDatabase class

---
+++ app/src/main/java/ru/buylist/data/db/BuyListDatabase.kt
@@ -0,0 +1,55 @@
+package ru.buylist.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import ru.buylist.data.dao.BuyListDao
+import ru.buylist.data.dao.CategoryDao
+import ru.buylist.data.dao.GlobalItemDao
+import ru.buylist.data.dao.PatternDao
+import ru.buylist.data.entity_old.Category
+import ru.buylist.data.entity_old.Collection
+import ru.buylist.data.entity_old.GlobalItem
+import ru.buylist.data.entity_old.Item
+import ru.buylist.utils.AppExecutors
+import ru.buylist.utils.CategoryDatabaseWorker
+
+@Database(entities = [Collection::class, Item::class, GlobalItem::class, Category::class],
+        version = 1, exportSchema = false)
+abstract class BuyListDatabase : RoomDatabase() {
+    abstract fun buyListDao(): BuyListDao
+    abstract fun patternDao(): PatternDao
+    abstract fun globalItemDao(): GlobalItemDao
+    abstract fun categoryDao(): CategoryDao
+
+    companion object {
+        private const val DATABASE_NAME = "buyListBase"
+
+        @Volatile private var instance: BuyListDatabase? = null
+
+        fun getInstance(context: Context, executors: AppExecutors): BuyListDatabase {
+            return instance ?: synchronized(this) {
+                instance
+                        ?: buildDatabase(context, executors).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context, executors: AppExecutors): BuyListDatabase {
+            return Room.databaseBuilder(context, BuyListDatabase::class.java, DATABASE_NAME)
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            executors.discIO().execute {
+                                val categories = CategoryDatabaseWorker.getStandardCategories(context)
+                                for (category in categories) {
+                                    getInstance(context, executors).categoryDao().insertCategory(category)
+                                }
+                            }
+                        }
+                    })
+                    .build()
+        }
+    }
+}
