00ab0e92122b4ae800cd0b2b3e3f95ffdea900f1
Factis <53155715qwerty>
2020-05-28 18:26:36 +0800

    update InjectorUtils

--- app/src/main/java/ru/buylist/utils/InjectorUtils.kt
+++ app/src/main/java/ru/buylist/utils/InjectorUtils.kt
@@ -2,13 +2,14 @@ package ru.buylist.utils
 
 import android.content.Context
 import ru.buylist.BuyListApp
+import ru.buylist.data.repositories.buyList.BuyListDataSource
 import ru.buylist.data.repositories.buyList.BuyListRepository
 
 object InjectorUtils {
 
     fun getExecutors(): AppExecutors = AppExecutors()
 
-    private fun getBuyListRepository(context: Context) : BuyListRepository {
+    private fun getBuyListRepository(context: Context) : BuyListDataSource {
         return BuyListRepository.getInstance(
                 getExecutors(),
                 (context.applicationContext as BuyListApp).getDatabase().buyListDao())
