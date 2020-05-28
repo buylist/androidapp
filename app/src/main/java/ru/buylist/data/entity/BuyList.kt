8860b2f5a43b720714ca8b7793c64f631a3d029c
Factis <53155715qwerty>
2020-05-28 17:11:14 +0800

    renamed

--- app/src/main/java/ru/buylist/data/entity/BuyList.kt
+++ app/src/main/java/ru/buylist/data/entity/BuyList.kt
@@ -3,8 +3,8 @@ package ru.buylist.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "lists")
-data class List(
+@Entity(tableName = "buy_lists")
+data class BuyList(
         @PrimaryKey var id: Long = System.currentTimeMillis(),
         var title: String = "",
         var items: String = ""
