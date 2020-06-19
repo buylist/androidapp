5483f6e596b888d7a45dda37eb7fcea516626bfc
Factis <53155715qwerty>
2020-06-19 18:11:40 +0800

    added default value

--- app/src/main/java/ru/buylist/data/entity/Item.kt
+++ app/src/main/java/ru/buylist/data/entity/Item.kt
@@ -3,11 +3,11 @@ package ru.buylist.data.entity
 
 data class Item(
         var id: Long = System.currentTimeMillis(),
-        var globalItemId: Long,
+        var globalItemId: Long = 0,
         var name: String,
-        var quantity: String,
+        var quantity: String = "",
         var isPurchased: Boolean = false,
-        var category: Category
+        var category: Category = Category()
 ) {
     val isEmpty get() = name.isEmpty()
 }
