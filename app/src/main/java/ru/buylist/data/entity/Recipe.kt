2c4f8ad7bd5a9aa8f392e64932480a4edf91b1eb
Factis <53155715qwerty>
2020-08-21 14:56:12 +0800

    добавлены дополнительные поля

--- app/src/main/java/ru/buylist/data/entity/Recipe.kt
+++ app/src/main/java/ru/buylist/data/entity/Recipe.kt
@@ -6,7 +6,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "recipes")
 data class Recipe(
         @PrimaryKey var id: Long = System.currentTimeMillis(),
-        var title: String = "",
+        var title: String = "(Без названия...)",
         var items: String = "",
-        var description: String = ""
+        var description: String = "",
+        var image: String = "",
+        var category: String = "",
+        var cookingTime: String = "",
+        var portion: String = ""
 )
