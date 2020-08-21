bb0653bf2c56dcf9961320e0b2cd0ee837ecc144
Factis <53155715qwerty>
2020-08-21 19:24:25 +0800

    замена CircularRevealLinearLayout на MaterialContainerTransform

--- app/src/main/java/ru/buylist/presentation/fragments/RecipesFragment.kt
+++ app/src/main/java/ru/buylist/presentation/fragments/RecipesFragment.kt
@@ -1,10 +1,14 @@
 package ru.buylist.presentation.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.TransitionManager
+import com.google.android.material.transition.MaterialContainerTransform
 import kotlinx.android.synthetic.main.activity_fragment.*
 import kotlinx.android.synthetic.main.fragment_recipes.*
 import ru.buylist.R
@@ -73,15 +77,37 @@ class RecipesFragment : BaseFragment<FragmentRecipesBinding>() {
     }
 
     private fun expandFab() {
-        fab_add.isExpanded = true
-        requireActivity().nav_bottom.visibility = View.GONE
+        val transition = buildContainerTransform().apply {
+            startView = fab_add
+            endView = layout_new_item
+            addTarget(layout_new_item)
+        }
+        TransitionManager.beginDelayedTransition(coordinator_layout, transition)
+        layout_new_item.visibility = View.VISIBLE
         shadow_view.visibility = View.VISIBLE
+        fab_add.visibility = View.GONE
+        requireActivity().nav_bottom.visibility = View.GONE
         field_name.requestFocus()
     }
 
     private fun minimizeFab() {
-        fab_add.isExpanded = false
-        requireActivity().nav_bottom.visibility = View.VISIBLE
+        val transition = buildContainerTransform().apply {
+            startView = layout_new_item
+            endView = fab_add
+            addTarget(fab_add)
+        }
+        TransitionManager.beginDelayedTransition(coordinator_layout, transition)
+        layout_new_item.visibility = View.GONE
         shadow_view.visibility = View.GONE
+        fab_add.visibility = View.VISIBLE
+        requireActivity().nav_bottom.visibility = View.VISIBLE
     }
+
+    private fun buildContainerTransform() =
+            MaterialContainerTransform().apply {
+                scrimColor = Color.TRANSPARENT
+                duration = 500
+                fadeMode = MaterialContainerTransform.FADE_MODE_IN
+                interpolator = FastOutSlowInInterpolator()
+            }
 }
