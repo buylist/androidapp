feaa125ca109795e2cd704648138ccca592cbb4e
Factis <53155715qwerty>
2020-08-25 17:35:48 +0800

    конвертация шагов приготовления из / в json

--- app/src/main/java/ru/buylist/utils/JsonUtils.kt
+++ app/src/main/java/ru/buylist/utils/JsonUtils.kt
@@ -2,12 +2,12 @@ package ru.buylist.utils
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import ru.buylist.data.entity.CookingStep
 import ru.buylist.data.entity.Item
 
 object JsonUtils {
 
-    fun convertItemsToJson(items: List<Item>) : String = Gson().toJson(items)
-
+    fun convertItemsToJson(items: List<Item>): String = Gson().toJson(items)
 
     fun convertItemsFromJson(items: String): List<Item> {
         if (items.isEmpty()) {
@@ -15,5 +15,14 @@ object JsonUtils {
         }
         val type = object : TypeToken<List<Item>>() {}.type
         return Gson().fromJson(items, type)
+    }
+
+    fun convertCookingStepsToJson(steps: List<CookingStep>): String = Gson().toJson(steps)
+
+    fun convertCookingStepsFromJson(steps: String): List<CookingStep> {
+        if (steps.isEmpty()) return emptyList()
+
+        val type = object : TypeToken<List<CookingStep>>() {}.type
+        return Gson().fromJson(steps, type)
     }
 }
