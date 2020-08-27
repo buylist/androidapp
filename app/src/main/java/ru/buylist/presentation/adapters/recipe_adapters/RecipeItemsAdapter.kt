e46b8b7488381a3bffa19256b9c033ed9d5d7abf
Factis <53155715qwerty>
2020-08-27 14:58:50 +0800

    добавлен binding

--- app/src/main/java/ru/buylist/presentation/adapters/recipe_adapters/RecipeItemsAdapter.kt
+++ app/src/main/java/ru/buylist/presentation/adapters/recipe_adapters/RecipeItemsAdapter.kt
@@ -2,20 +2,22 @@ package ru.buylist.presentation.adapters.recipe_adapters
 
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import ru.buylist.R
 import ru.buylist.data.entity.wrappers.ItemWrapper
+import ru.buylist.databinding.ItemBuyListDetailBinding
 import ru.buylist.presentation.adapters.GenericViewHolder
 
 class RecipeItemsAdapter(var items: List<ItemWrapper>) : ListAdapter<ItemWrapper, GenericViewHolder>(RecipeItemsDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_buy_list_detail, parent, false)
-        return RecipeItemsViewHolder(view)
+        val binding: ItemBuyListDetailBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_buy_list_detail,
+                parent, false)
+        return RecipeItemsViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: GenericViewHolder, position: Int) {
@@ -27,17 +29,20 @@ class RecipeItemsAdapter(var items: List<ItemWrapper>) : ListAdapter<ItemWrapper
         submitList(items)
     }
 
-    private inner class RecipeItemsViewHolder(itemView: View) : GenericViewHolder(itemView) {
-        val circle: ImageView = itemView.findViewById(R.id.img_category_circle)
-        val itemName: TextView = itemView.findViewById(R.id.tv_item_title)
+
+    // ViewHolder
+    private inner class RecipeItemsViewHolder(val binding: ItemBuyListDetailBinding) : GenericViewHolder(binding.root) {
+
         override fun bind(position: Int) {
-            circle.setColorFilter(Color.parseColor(items[position].item.category.color))
-            itemName.text = items[position].item.name
+            binding.item = items[position]
+            binding.imgCategoryCircle.setColorFilter(Color.parseColor(items[position].item.category.color))
         }
 
     }
 }
 
+
+// DiffUtil
 class RecipeItemsDiffCallback : DiffUtil.ItemCallback<ItemWrapper>() {
     override fun areItemsTheSame(oldItem: ItemWrapper, newItem: ItemWrapper): Boolean {
         return oldItem.item.id == newItem.item.id
