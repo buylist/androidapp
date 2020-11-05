eddba3204dcd707206a5bb30f6f92e9ff283185f
Factis <53155715qwerty>
2020-11-05 22:22:01 +0800

    Добавлен модификатор suspend.

--- app/src/main/java/ru/buylist/data/dao/RecipeDao.kt
+++ app/src/main/java/ru/buylist/data/dao/RecipeDao.kt
@@ -8,16 +8,16 @@ import ru.buylist.data.entity.Recipe
 interface RecipeDao {
 
     @Insert
-    fun insertRecipe(recipe: Recipe)
+    suspend fun insertRecipe(recipe: Recipe)
 
     @Update
-    fun updateRecipe(recipe: Recipe)
+    suspend fun updateRecipe(recipe: Recipe)
 
     @Delete
-    fun deleteRecipe(recipe: Recipe)
+    suspend fun deleteRecipe(recipe: Recipe)
 
     @Delete
-    fun deleteSelectedRecipes(recipes: List<Recipe>)
+    suspend fun deleteSelectedRecipes(recipes: List<Recipe>)
 
     @Query("SELECT * FROM recipes")
     fun observeRecipes(): LiveData<List<Recipe>>
@@ -26,12 +26,12 @@ interface RecipeDao {
     fun observeRecipeById(recipeId: Long): LiveData<Recipe>
 
     @Query("DELETE FROM recipes")
-    fun deleteAllRecipes()
+    suspend fun deleteAllRecipes()
 
     @Query("SELECT * FROM recipes")
-    fun getRecipes(): List<Recipe>
+    suspend fun getRecipes(): List<Recipe>
 
     @Query("SELECT * FROM recipes WHERE id = :recipeId")
-    fun getRecipe(recipeId: Long): Recipe
+    suspend fun getRecipe(recipeId: Long): Recipe
 
 }
