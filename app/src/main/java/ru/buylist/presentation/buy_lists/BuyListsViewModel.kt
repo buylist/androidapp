98d16e2a7d6b494791dfb963e43db3f75a2fde00
Factis <53155715qwerty>
2020-11-13 00:29:45 +0800

    Observable заменены на LiveData.

--- app/src/main/java/ru/buylist/presentation/buy_lists/BuyListsViewModel.kt
+++ app/src/main/java/ru/buylist/presentation/buy_lists/BuyListsViewModel.kt
@@ -1,10 +1,10 @@
 package ru.buylist.presentation.buy_lists
 
-import androidx.databinding.ObservableBoolean
-import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import ru.buylist.R
+import ru.buylist.data.Result
+import ru.buylist.data.Result.*
 import ru.buylist.data.entity.BuyList
 import ru.buylist.data.entity.wrappers.BuyListWrapper
 import ru.buylist.data.repositories.buyList.BuyListsDataSource
@@ -15,13 +15,33 @@ import ru.buylist.utils.Event
  */
 class BuyListsViewModel(private val repository: BuyListsDataSource) : ViewModel() {
 
-    var listIsEmpty = ObservableBoolean(true)
-    var fabIsShown = ObservableBoolean(true)
-    var isEditable: Boolean = false
-    var buyListTitle = ObservableField("")
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+    private val _buyListToEdit = MutableLiveData<Int>()
 
-    var wrapperList = MutableLiveData<List<BuyListWrapper>>().apply { value = emptyList() }
-    var buyLists = mutableListOf<BuyList>()
+    private val _triggers = MediatorLiveData<Pair<Boolean?, Int?>>().apply {
+        addSource(_forceUpdate) { value = Pair(it, _buyListToEdit.value) }
+        addSource(_buyListToEdit) { value = Pair(_forceUpdate.value, it) }
+    }
+
+    private val _buyLists: LiveData<List<BuyListWrapper>> = _triggers.switchMap { triggers ->
+        if (triggers.first == true) {
+            TODO("Load buy lists from remote data source.")
+        }
+        repository.observeBuyLists().distinctUntilChanged().switchMap { loadBuyLists(it, triggers.second) }
+    }
+
+    val buyLists: LiveData<List<BuyListWrapper>> = _buyLists
+
+    val listIsEmpty: LiveData<Boolean> = Transformations.map(_buyLists) {
+        it.isEmpty()
+    }
+
+    val fabIsShown = MutableLiveData<Boolean>(true)
+
+    val buyListTitle = MutableLiveData<String>()
+
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarText: LiveData<Event<Int>> = _snackbarText
 
     private val _addBuyListsEvent = MutableLiveData<Event<Unit>>()
     val addBuyList: LiveData<Event<Unit>> = _addBuyListsEvent
@@ -32,10 +52,6 @@ class BuyListsViewModel(private val repository: BuyListsDataSource) : ViewModel(
     private val _detailsEvent = MutableLiveData<Event<BuyList>>()
     val detailsEvent: LiveData<Event<BuyList>> = _detailsEvent
 
-    init {
-        loadList()
-    }
-
     fun addNewBuyList() {
         _addBuyListsEvent.value = Event(Unit)
     }
@@ -45,11 +61,14 @@ class BuyListsViewModel(private val repository: BuyListsDataSource) : ViewModel(
     }
 
     fun save() {
-        val buyList = BuyList(title = buyListTitle.get().toString())
-        repository.saveBuyList(buyList)
-        buyLists.add(buyList)
-        updateUi()
-        buyListTitle.set("")
+        val title = buyListTitle.value
+        if (title == null) {
+            showSnackbarMessage(R.string.snackbar_empty_buy_list_title)
+            return
+        }
+
+        createBuyList(BuyList(title = title))
+        buyListTitle.value = null
         hideLayoutWithFields()
     }
 
@@ -57,89 +76,66 @@ class BuyListsViewModel(private val repository: BuyListsDataSource) : ViewModel(
         _buyListCreated.value = Event((Unit))
     }
 
-    fun saveEditedData(buyListWrapper: BuyListWrapper, newTitle: String) {
-        val items = getItems()
-        buyListWrapper.buyList.title = newTitle
-        updateBuyListWrapper(items, buyListWrapper.buyList, buyListWrapper.position, false)
-        repository.updateBuyList(buyListWrapper.buyList)
-        isEditable = false
-    }
-
     fun edit(buyListWrapper: BuyListWrapper) {
-        val items = getItems()
-        checkEditableField(items)
-        updateBuyListWrapper(items, buyListWrapper.buyList, buyListWrapper.position, true)
-        isEditable = true
+        _buyListToEdit.value = buyListWrapper.position
     }
 
-    fun delete(buyListWrapper: BuyListWrapper) {
-        buyLists.remove(buyListWrapper.buyList)
-        repository.deleteBuyList(buyListWrapper.buyList)
-        updateUi()
+    fun saveEditedData(buyListWrapper: BuyListWrapper, newTitle: String) {
+        buyListWrapper.buyList.title = newTitle
+        viewModelScope.launch {
+            repository.updateBuyList(buyListWrapper.buyList)
+        }
+        _buyListToEdit.value = null
+        fabIsShown.value = true
     }
+
+    fun delete(buyListWrapper: BuyListWrapper) = viewModelScope.launch {
+        repository.deleteBuyList(buyListWrapper.buyList)
+    }
+
 
     fun showHideFab(dy: Int) {
-        if (isEditable) {
-            fabIsShown.set(false)
+        if (_buyListToEdit.value != null) {
+            fabIsShown.value = false
             return
         }
-        fabIsShown.set(dy <= 0)
+        fabIsShown.value = (dy <= 0)
     }
 
-    private fun updateUi() {
-        val items = getWrapperList(buyLists)
-        listIsEmpty.set(items.isEmpty())
-        wrapperList.value = items
+    private fun createBuyList(buyList: BuyList) = viewModelScope.launch {
+        repository.saveBuyList(buyList)
     }
 
-    private fun updateBuyListWrapper(items: MutableList<BuyListWrapper>, buyList: BuyList, position: Int,
-                                     isEditable: Boolean = false, isSelected: Boolean = false) {
-        items.removeAt(position)
-        items.add(position, BuyListWrapper(buyList, position, isEditable, isSelected))
-        wrapperList.value = items
-    }
-
-    private fun checkEditableField(items: MutableList<BuyListWrapper>) {
-        if (isEditable) {
-            val iterator = items.iterator()
-            while (iterator.hasNext()) {
-                val item = iterator.next()
-                if (item.isEditable) {
-                    updateBuyListWrapper(items, item.buyList, item.position)
-                    break
-                }
-            }
-        }
-    }
-
-    private fun getItems(): MutableList<BuyListWrapper> {
-        val items: MutableList<BuyListWrapper> = mutableListOf()
-        wrapperList.value?.let { items.addAll(it) }
-        return items
-    }
-
-    private fun getWrapperList(buyLists: List<BuyList>): List<BuyListWrapper> {
+    private fun getWrapperList(buyLists: List<BuyList>, position: Int?)
+            : List<BuyListWrapper> {
         val newList = mutableListOf<BuyListWrapper>()
         for ((i, buyList) in buyLists.withIndex()) {
             val buyListWrapper = BuyListWrapper(buyList, i)
+            if (position != null && position == i) {
+                buyListWrapper.isEditable = true
+            }
             newList.add(buyListWrapper)
         }
         return newList
     }
 
-    private fun loadList() {
-        repository.getBuyLists(object : BuyListsDataSource.LoadBuyListsCallback {
-            override fun onBuyListsLoaded(buyLists: List<BuyList>) {
-                wrapperList.value = getWrapperList(buyLists).toMutableList()
-                this@BuyListsViewModel.buyLists.clear()
-                this@BuyListsViewModel.buyLists.addAll(buyLists)
-                listIsEmpty.set(false)
-            }
-
-            override fun onDataNotAvailable() {
-                wrapperList.value = emptyList()
-                listIsEmpty.set(true)
-            }
-        })
+    private fun showSnackbarMessage(message: Int) {
+        _snackbarText.value = Event(message)
     }
+
+    private fun loadBuyLists(buyListResult: Result<List<BuyList>>, position: Int?)
+            : LiveData<List<BuyListWrapper>> {
+        val result = MutableLiveData<List<BuyListWrapper>>()
+
+        if (buyListResult is Success) {
+            viewModelScope.launch {
+                result.value = getWrapperList(buyListResult.data, position)
+            }
+        } else {
+            result.value = emptyList()
+            showSnackbarMessage(R.string.snackbar_buy_lists_loading_error)
+        }
+        return result
+    }
+
 }
