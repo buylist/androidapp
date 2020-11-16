b65d84d80a075565bef4bc866b7c065e204acff5
Factis <53155715qwerty>
2020-11-16 18:11:42 +0800

    Удалены executors.

--- app/src/main/java/ru/buylist/utils/InjectorUtils.kt
+++ app/src/main/java/ru/buylist/utils/InjectorUtils.kt
@@ -42,7 +42,6 @@ object InjectorUtils {
 
     private fun getPatternsRepository(): PatternsDataSource {
         return PatternsRepository.getInstance(
-                getExecutors(),
                 BuyListApp.get().getDatabase().patternDao())
     }
 
